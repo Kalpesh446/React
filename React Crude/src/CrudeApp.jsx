7e02f9dc94c84783b53b39d7cb0b5ef909ee39c0
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-04-19 20:40:56 +0530

    new input add

--- React Crude/src/CrudeApp.jsx
+++ React Crude/src/CrudeApp.jsx
@@ -2,62 +2,108 @@ import React, { useEffect, useState } from "react";
 import "./index.css";
 
 const CrudeApp = () => {
-  const [data, setData] = useState({ name: "", email: "" });
-  const [user, setUser] = useState(() => {
-    const storedData = localStorage.getItem("userData");
-    return storedData ? JSON.parse(storedData) : [];
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+    age: "",
+    gender: "",
+    interests: [],
+    birthData: "",
   });
-  const [editingIndex, setEditingIndex] = useState(null);
 
-  useEffect(() => {
-    localStorage.setItem("userData", JSON.stringify(user));
-  }, [user]);
+  // const [data, setData] = useState({ name: "", email: "" });
+  // const [user, setUser] = useState(() => {
+  //   const storedData = localStorage.getItem("userData");
+  //   return storedData ? JSON.parse(storedData) : [];
+  // });
+  // const [editingIndex, setEditingIndex] = useState(null);
 
-  const handleChange = (e) => {
-    setData({ ...data, [e.target.name]: e.target.value });
-  };
+  // useEffect(() => {
+  //   localStorage.setItem("userData", JSON.stringify(user));
+  // }, [user]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (data.name === "" || data.email === "") {
-      alert("Please fill in all fields");
-    } else if (editingIndex != null && editingIndex !== undefined) {
-      const updatedUser = [...user];
-      updatedUser[editingIndex] = data;
-      setUser(updatedUser);
-      setEditingIndex(null);
-    } else {
-      setUser([...user, data]);
-    }
-    setData({ name: "", email: "" });
-  };
+  // const handleChange = (e) => {
+  //   setData({ ...data, [e.target.name]: e.target.value });
+  // };
 
-  const handleAllDelete = () => {
-    setUser([]);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (data.name === "" || data.email === "") {
+  //     alert("Please fill in all fields");
+  //   } else if (editingIndex != null && editingIndex !== undefined) {
+  //     const updatedUser = [...user];
+  //     updatedUser[editingIndex] = data;
+  //     setUser(updatedUser);
+  //     setEditingIndex(null);
+  //   } else {
+  //     setUser([...user, data]);
+  //   }
+  //   setData({ name: "", email: "" });
+  // };
 
-  const handleDelete = (i) => {
-    const updatedUser = user.filter((e, index) => index !== i);
-    setUser(updatedUser);
-  };
+  // const handleAllDelete = () => {
+  //   setUser([]);
+  // };
 
-  const handleEdit = (e, i) => {
-    setData({ ...data, name: e.name, email: e.email });
-    setEditingIndex(i);
-  };
+  // const handleDelete = (i) => {
+  //   const updatedUser = user.filter((e, index) => index !== i);
+  //   setUser(updatedUser);
+  // };
+
+  // const handleEdit = (e, i) => {
+  //   setData({ ...data, name: e.name, email: e.email });
+  //   setEditingIndex(i);
+  // };
 
   return (
     <>
       <h1 className="text-center">React</h1>
-      <form className="w-50 m-auto" onSubmit={handleSubmit}>
+      <form className="w-50 m-auto">
         <div className="mb-3">
-          <label htmlFor="" className="form-label">
-            Name
-          </label>
-          <input className="form-control" type="text" name="name" id="" value={data.name} onChange={handleChange} />
+          <div>
+            <label htmlFor="firstName" className="form-label">
+              First Name:
+            </label>
+            <input className="form-control" type="text" name="firstName" value={formData.firstName} placeholder="Enter your first name" />
+          </div>
           <br />
-          <label htmlFor="">Email</label>
-          <input className="form-control" type="email" name="email" id="" value={data.email} onChange={handleChange} />
+          <div>
+            <label htmlFor="lastName">Last Name:</label>
+            <input className="form-control" type="text" name="lastName" value={formData.lastName} placeholder="Enter your last name" />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input className="form-control" type="email" name="email" value={formData.email} placeholder="Enter your Email Id" />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="phoneNumber">Phone Number:</label>
+            <input className="form-control" type="number" name="phoneNumber" value={formData.phoneNumber} placeholder="Enter your Phone Number" />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input className="form-control" type="password" name="password" value={formData.password} placeholder="Enter your password" />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="password">Confirm Password:</label>
+            <input className="form-control" type="password" name="confirmPassword" value={formData.confirmPassword} placeholder="Enter your confirm password" />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="gender">Gender :</label>
+            <select name="gender" value={formData.gender}>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+          </div>
           <br />
           <button type="submit" className="btn btn-primary">
             submit
@@ -65,7 +111,7 @@ const CrudeApp = () => {
         </div>
       </form>
 
-      {user.length > 0 ? (
+      {/* {user.length > 0 ? (
         <table className="table w-50 m-auto table-striped table-responsive-lg border">
           <thead>
             <tr>
@@ -102,7 +148,7 @@ const CrudeApp = () => {
         </table>
       ) : (
         ""
-      )}
+      )} */}
     </>
   );
 };
