81450c0a36ab2b01cde92fca934277440ce376ff
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-04-30 19:53:32 +0530

    handle form with formik

--- reactvalidation/src/Registration.jsx
+++ reactvalidation/src/Registration.jsx
@@ -1,8 +1,21 @@
-import React from "react";
 import styled from "styled-components";
 import { GlobalStyle } from "./Styles/globalStyles";
+import { useFormik } from "formik";
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirm_password: "",
+};
 
 const Registration = () => {
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues: initialValues,
+    onSubmit: (values) => {
+      console.log("🚀 ~   file: Registration.jsx:9 ~   Registration ~   values:", values);
+    },
+  });
   return (
     <>
       <GlobalStyle />
@@ -12,35 +25,40 @@ const Registration = () => {
             <div className="modal-container">
               <div className="modal-left">
                 <h1 className="modal-title">Welcome!</h1>
-                <p className="modal-desc">To the thapa technical website for programmers.</p>
-                <form>
+                <p className="modal-desc">To the website for programmers.</p>
+                <form onSubmit={handleSubmit}>
                   <div className="input-block">
                     <label htmlFor="name" className="input-label">
                       Name
                     </label>
-                    <input type="name" autoComplete="off" name="name" id="name" placeholder="Name" />
-                    {/* {errors.name && touched.name ? <p className="form-error">{errors.name}</p> : null} */}
+                    <input type="name" autoComplete="off" name="name" id="name" placeholder="Name" value={values.name} onChange={handleChange} onBlur={handleBlur} />
                   </div>
                   <div className="input-block">
                     <label htmlFor="email" className="input-label">
                       Email
                     </label>
-                    <input type="email" autoComplete="off" name="email" id="email" placeholder="Email" />
-                    {/* {errors.email && touched.email ? <p className="form-error">{errors.email}</p> : null} */}
+                    <input type="email" autoComplete="off" name="email" id="email" placeholder="Email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
                   </div>
                   <div className="input-block">
                     <label htmlFor="password" className="input-label">
                       Password
                     </label>
-                    <input type="password" autoComplete="off" name="password" id="password" placeholder="Password" />
-                    {/* {errors.password && touched.password ? <p className="form-error">{errors.password}</p> : null} */}
+                    <input type="password" autoComplete="off" name="password" id="password" placeholder="Password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
                   </div>
                   <div className="input-block">
                     <label htmlFor="confirm_password" className="input-label">
                       Confirm Password
                     </label>
-                    <input type="password" autoComplete="off" name="confirm_password" id="confirm_password" placeholder="Confirm Password" />
-                    {/* {errors.confirm_password && touched.confirm_password ? <p className="form-error">{errors.confirm_password}</p> : null} */}
+                    <input
+                      type="password"
+                      autoComplete="off"
+                      name="confirm_password"
+                      id="confirm_password"
+                      placeholder="Confirm Password"
+                      value={values.confirm_password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
                   </div>
                   <div className="modal-buttons">
                     <a href="#" className="">
