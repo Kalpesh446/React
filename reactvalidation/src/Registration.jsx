44670e3aab01ffc0e06747dbcd565392556d20ef
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-05-01 16:27:53 +0530

    changes

--- reactvalidation/src/Registration.jsx
+++ reactvalidation/src/Registration.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import { GlobalStyle } from "./Styles/globalStyles";
 import { useFormik } from "formik";
@@ -12,13 +13,15 @@ const initialValues = {
 
 const Registration = () => {
   const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
-    initialValues: initialValues,
+    initialValues,
     validationSchema: signUpSchema,
-    onSubmit: (values) => {
-      console.log("🚀 ~   file: Registration.jsx:9 ~   Registration ~   values:", values);
+    onSubmit: (values, action) => {
+      console.log("🚀 ~ file: Registration.jsx ~ line 11 ~ Registration ~ values", values);
+      action.resetForm();
     },
   });
-  console.log("🚀 ~   file: Registration.jsx:22 ~   Registration ~   errors:", errors);
+  console.log("🚀 ~ file: Registration.jsx ~ line 25 ~ Registration ~ errors", errors);
+
   return (
     <>
       <GlobalStyle />
@@ -28,25 +31,28 @@ const Registration = () => {
             <div className="modal-container">
               <div className="modal-left">
                 <h1 className="modal-title">Welcome!</h1>
-                <p className="modal-desc">To the website for programmers.</p>
+                <p className="modal-desc">To the thapa technical website for programmers.</p>
                 <form onSubmit={handleSubmit}>
                   <div className="input-block">
                     <label htmlFor="name" className="input-label">
                       Name
                     </label>
                     <input type="name" autoComplete="off" name="name" id="name" placeholder="Name" value={values.name} onChange={handleChange} onBlur={handleBlur} />
+                    {errors.name && touched.name ? <p className="form-error">{errors.name}</p> : null}
                   </div>
                   <div className="input-block">
                     <label htmlFor="email" className="input-label">
                       Email
                     </label>
                     <input type="email" autoComplete="off" name="email" id="email" placeholder="Email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
+                    {errors.email && touched.email ? <p className="form-error">{errors.email}</p> : null}
                   </div>
                   <div className="input-block">
                     <label htmlFor="password" className="input-label">
                       Password
                     </label>
                     <input type="password" autoComplete="off" name="password" id="password" placeholder="Password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+                    {errors.password && touched.password ? <p className="form-error">{errors.password}</p> : null}
                   </div>
                   <div className="input-block">
                     <label htmlFor="confirm_password" className="input-label">
@@ -62,6 +68,7 @@ const Registration = () => {
                       onChange={handleChange}
                       onBlur={handleBlur}
                     />
+                    {errors.confirm_password && touched.confirm_password ? <p className="form-error">{errors.confirm_password}</p> : null}
                   </div>
                   <div className="modal-buttons">
                     <a href="#" className="">
--- reactvalidation/src/schemas/index.jsx
+++ reactvalidation/src/schemas/index.jsx
@@ -3,7 +3,7 @@ import * as Yup from "yup";
 export const signUpSchema = Yup.object({
   name: Yup.string().min(2).max(25).required("Please enter your Name"),
   email: Yup.string().email().required("Please enter your Email"),
-  password: Yup.string().min(8).required("Please enter your password"),
+  password: Yup.string().min(6).required("Please enter your password"),
   confirm_password: Yup.string()
     .required("Please enter your confirm password")
     .oneOf([Yup.ref("password"), null], "Password must match"),
