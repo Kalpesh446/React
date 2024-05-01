b929c445d13f2caa524a9e9ea7f3b0da12bdc9e5
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-05-01 17:54:54 +0530

    validation with formik and yup

--- reactValidation-main/src/Registration.jsx
+++ reactValidation-main/src/Registration.jsx
@@ -84,10 +84,7 @@ const Registration = () => {
                 </p>
               </div>
               <div className="modal-right">
-                <img
-                  src="https://images.unsplash.com/photo-1512486130939-2c4f79935e4f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dfd2ec5a01006fd8c4d7592a381d3776&auto=format&fit=crop&w=1000&q=80"
-                  alt=""
-                />
+                <img src="https://images.unsplash.com/photo-1566888596782-c7f41cc184c5?ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80" alt="" />
               </div>
             </div>
           </div>
