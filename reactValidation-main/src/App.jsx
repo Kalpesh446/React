82362686bdaa697fd59ee25f7407c8e117ea078b
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-05-01 21:16:14 +0530

    react router dom add

--- reactValidation-main/src/App.jsx
+++ reactValidation-main/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 // import SignUp from "./SignUp";
+import { Route, Routes } from "react-router-dom";
 import Registration from "./Registration";
 
 const App = () => {
   // return <SignUp />;
-  return <Registration />;
+  return (
+    <Routes>
+      <Route exact path="/" element={<Registration />}></Route>
+      {/* <Route exact path="/user-table" element={<R />}></Route> */}
+    </Routes>
+  );
 };
 
 export default App;
