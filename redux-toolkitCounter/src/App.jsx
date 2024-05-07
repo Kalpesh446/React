2c5eaabc58d4a03f35b243bb34d9dff03af396f7
Kalpesh446 <Kalpeshchavda446@gmail.com>
2024-05-07 18:19:46 +0530

    action call from slice through dispatch and import action

--- redux-toolkitCounter/src/App.jsx
+++ redux-toolkitCounter/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { increment, decrement, incrementByAmount, multiply } from "./redux/counter/counterSlice";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Navbar />
+      <div className="container">
+        <button className="button" onClick={() => dispatch(decrement())}>
+          -
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="main-content">This is main {count}</div>
+        <button className="button" onClick={() => dispatch(increment())}>
+          +
+        </button>
+        <button className="button" onClick={() => dispatch(multiply())}>
+          *
+        </button>
+        <button className="button" onClick={() => dispatch(incrementByAmount(5))}>
+          incrementByAmount (5)
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
